6669fa97f09f8f10e6ef5b746ddfc98d619b2cfe
Jahan Syed <jahansyed@hotmail.co.uk>
2016-04-15 09:24:03 +0100

    Race condition found - fixed by passing a local var to go thread

--- go_thread_no_control.go
+++ go_thread_no_control.go
@@ -8,11 +8,11 @@ func main() {
 	maxroutines := make(chan int, 5)
 	for i := 0; i < 20; i++ {
 		maxroutines <- i
-		go func() {
-			fmt.Println(i)
+		go func(j int) {
+			fmt.Println(j)
 			time.Sleep(1000 * time.Millisecond)
 
-		}()
+		}(i)
 		<- maxroutines
 	}
 
