862f910f63c22d8dd00dbeaaaf84d4fdc9fee030
jahantech <jahansyed@hotmail.co.uk>
2017-11-06 21:40:59 +0000

    skeleton for status checker

---
+++ statusChecker/main.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+--url : URL for the status end point
+--status : Status code for the above URL
+--checks : Number of checks performed
+--interval : Wait between each check
+--port : If URL is not given, a connection to tcp should be used for status
+*/
+func main() {
+
+	return
+}
